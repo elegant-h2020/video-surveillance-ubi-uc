ead7ab857cc06b68a976d392b6e4f98e2d9a0c84
vmatsoukas <vmatsoukas@ubitech.eu>
2024-01-30 11:13:59 +0200

    replace flink with nebulastream operations

--- video-stream-processor/src/main/java/eu/ubitech/video/app/flink/processor/VideoStreamProcessor.java
+++ video-stream-processor/src/main/java/eu/ubitech/video/app/flink/processor/VideoStreamProcessor.java
@@ -1,24 +1,29 @@
 package eu.ubitech.video.app.flink.processor;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
 
 import eu.ubitech.video.app.flink.util.*;
-//import org.apache.flink.configuration.ConfigConstants;
-//import org.apache.flink.configuration.Configuration;
-import org.apache.flink.api.common.ExecutionConfig;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
-import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.DataStreamSink;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer010;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer010;
 import org.apache.log4j.Logger;
+import stream.nebula.exceptions.RESTException;
+import stream.nebula.operators.sinks.FileSink;
+import stream.nebula.runtime.NebulaStreamRuntime;
+import stream.nebula.runtime.Query;
+import stream.nebula.udf.MapFunction;
+
+import static stream.nebula.expression.Expressions.attribute;
 
 /**
- * Class to consume incoming JSON messages from Kafka and process them using Flink Structured Streaming.
- */
+ * Class to consume incoming JSON messages from Kafka and process
+ * them using Flink and OpenCV.
+ **/
 public class VideoStreamProcessor {
     private static final Logger logger = Logger.getLogger(VideoStreamProcessor.class);
 
@@ -54,37 +59,85 @@ public class VideoStreamProcessor {
 
         logger.info("Captured Frames need to contain a face in order to get ouput");
 
-        //Deserialising the data consumed from the topic "Video-stream-event" in kafka
+        //Deserializing the data consumed from the topic "Video-stream-event" in Kafka
         // using the properties file and JSonDeserializer
         FlinkKafkaConsumer010<VideoEventStringData> flinkConsumer = new FlinkKafkaConsumer010 <VideoEventStringData>(
                 envMap.get(EnvKeys.KAFKA_TOPIC.toString())
-                .toString(), new JsonDeserializer() , properties);
+                        .toString(), new JsonDeserializer() , properties);
 
+        //Send Generated JSON to output Kafka_Topic
         FlinkKafkaProducer010<String> flinkProducer = new FlinkKafkaProducer010<String>
                 (envMap.get(EnvKeys.KAFKA_TOPIC2.toString()).toString(),new SimpleStringSchema(), properties);
 
 
-        //set up output directory
+        //Set up output directory
         final String processedImageDir = envMap.get(EnvKeys.PROCC_OUT_DIR.toString());
         logger.warn("Output directory for saving processed images is set to "+ processedImageDir);
 
+        //Processing via Flink
         DataStreamSink<String> stream = env.addSource(flinkConsumer)
+                //keyBy(cameraID)
                 .keyBy("cameraId")
+                //The main proccessing part
                 .map(new TransformFunction())
+                //Filter frames with no faces
                 .filter(new FaceDataFilter())
+                //Send to Kafka
                 .addSink(flinkProducer)
                 ;
-
-
+        //Execute with Filnk
         if(execution_process.equals("FLINK")){
             env.execute();
         }
         //NES-ENABLEMENT
         else if(execution_process.equals("NES")) {
             logger.info("NES");
+//            NebulaStreamRuntime ner = NebulaStreamRuntime.getRuntime();
+//            ner.getConfig().setHost("212.101.173.11").setPort("8081");
+//            System.out.println(" *** connected:\n");
+//            nebulaStreamKafkaProcessing(ner);
         }
         else{
             logger.error("EXECUTION_PROCESS is not set properly, EXECUTION_PROCESS="+execution_process);
         }
     }
+//    public static void nebulaStreamKafkaProcessing(NebulaStreamRuntime ner) throws RESTException, IOException {
+//
+//        Query worker = ner.readFromSource("");
+//        worker.project(attribute("data"), attribute("cameraId"), attribute("timestamp"),
+//                        attribute("rows"), attribute("cols"), attribute("type"))
+//                .map(new FaceDetectionMapper());
+//        worker.sink(new FileSink("/output_kafka_FLINK_NES_query.csv", "CSV_FORMAT", true));
+//        int queryId = ner.executeQuery(worker, "BottomUp");
+//
+//        System.out.println("Query + " + queryId);
+//    }
+//
+//    static class InputEvent {
+//        String data;
+//        String cameraId;
+//        String timestamp;
+//        int rows;
+//        int cols;
+//        int type;
+//    }
+//
+//
+//    static class OutputEvent {
+//        String data;
+//        String cameraId;
+//        String timestamp;
+//        int rows;
+//        int cols;
+//        int type;
+//    }
+//
+//
+//    static class FaceDetectionMapper implements MapFunction<InputEvent, OutputEvent> {
+//        public OutputEvent map(final InputEvent inputEvent) {
+//            OutputEvent outputEvent =  new OutputEvent();
+//            // Mapping logic here
+//            return outputEvent;
+//        }
+//    }
 }
--- video-stream-similarity/src/main/java/eu/ubitech/video/app/similarity/processor/SimilarityProcessor.java
+++ video-stream-similarity/src/main/java/eu/ubitech/video/app/similarity/processor/SimilarityProcessor.java
@@ -1,6 +1,5 @@
 package eu.ubitech.video.app.similarity.processor;
 
-import com.fasterxml.jackson.databind.JsonDeserializer;
 import eu.ubitech.video.app.util.*;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
@@ -8,37 +7,50 @@ import org.apache.flink.streaming.api.datastream.DataStreamSink;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer010;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer010;
-import org.apache.kafka.clients.consumer.Consumer;
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.log4j.Logger;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.imgcodecs.Imgcodecs;
-import org.slf4j.LoggerFactory;
+import stream.nebula.exceptions.RESTException;
+import stream.nebula.operators.sinks.KafkaSink;
+import stream.nebula.runtime.NebulaStreamRuntime;
+import stream.nebula.runtime.Query;
+import stream.nebula.operators.sinks.FileSink;
+import stream.nebula.udf.MapFunction;
 
 import java.io.File;
 import java.io.IOException;
-import java.time.Duration;
 import java.util.*;
 
-import static eu.ubitech.video.app.similarity.processor.SimilarityProcessor.addInitialMember;
+import static stream.nebula.expression.Expressions.attribute;
 
+/**
+ *  Similarity-Processor module responsible for
+ *  feeding ML model with processed images
+ *  and collecting similarities results
+ */
 public class SimilarityProcessor {
     private static final Logger log = Logger.getLogger(SimilarityProcessor.class);
     private static  final FaceNetSmallV2Model faceNetSmallV2Model = new FaceNetSmallV2Model();
     public  static FaceSimilarity faceSimilarity = new FaceSimilarity();
 
 
+    //Load the OpenCV library
     static {
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
     }
 
+    /**Connect to Kafka,
+     * collect messages produced by video-stream-processor-module.
+     * Process them via Flink-Map operations
+     * Send output-results to Kafka
+     * @param args
+     * @throws Exception
+     */
     public static void main(String[] args) throws Exception {
 
-        //Add Flink enviroment
+        //Add Flink environment
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
         //Flink parameters reading
@@ -69,22 +81,28 @@ public class SimilarityProcessor {
         faceSimilarity.loadModel();
         addFacesFromImages();
 
-        //Deserialising the data consumed from the topic "Video-stream-event" in kafka using the properties file and JSonDeserializer
+        //Deserializing the data consumed from the topic "Video-stream-event" in kafka using the properties file and JSonDeserializer
         FlinkKafkaConsumer010<VideoEventStringProcessed> flinkConsumer = new FlinkKafkaConsumer010 <VideoEventStringProcessed>(
                 envMap.get(EnvKeys.KAFKA_TOPIC.toString()),
                 new VideoEventStringDeserializer(),
                 properties);
 
+        //Initializing the Flink-Kafka-producer:
+        //that output-data are sent to.
         FlinkKafkaProducer010<String> flinkProducer = new FlinkKafkaProducer010<String>(
                 envMap.get(EnvKeys.KAFKA_TOPIC2.toString()),
                 new SimpleStringSchema(),
                 properties);
 
 
-
-        DataStreamSink<String> stream = env.addSource(flinkConsumer)
-                .map(new SimilarityProcess())
-                .addSink(flinkProducer);
+        //Flink operations
+        DataStreamSink<String> stream =
+                //collect data from Kafka
+                env.addSource(flinkConsumer)
+                        //Use UDF-Similarity-Process
+                        .map(new SimilarityProcess())
+                        //Send Data to Kafka
+                        .addSink(flinkProducer);
 
 
 
@@ -94,10 +112,68 @@ public class SimilarityProcessor {
         //NES-ENABLEMENT
         else if(execution_process.equals("NES")) {
             log.info("NES");
+//            NebulaStreamRuntime ner = NebulaStreamRuntime.getRuntime();
+//            ner.getConfig().setHost("212.101.173.11").setPort("8081");
+//            System.out.println(" *** connected:\n");
+//            nebulaStreamKafkaProcessing(ner);
         }
         else{
             log.error("EXECUTION_PROCESS is not set properly, EXECUTION_PROCESS="+execution_process);
-        }    }
+        }
+    }
+//    public static void nebulaStreamKafkaProcessing(NebulaStreamRuntime ner) throws RESTException, IOException {
+//
+//        Query worker = ner.readFromSource("");
+//        worker.project(attribute("data"), attribute("cameraId"), attribute("timestamp"),
+//                        attribute("rows"), attribute("cols"), attribute("type"))
+//                .map(new SimilarityProcessMapper());
+//        worker.sink(new FileSink("/output_kafka_FLINK_NES_query.csv", "CSV_FORMAT", true));
+//        int queryId = ner.executeQuery(worker, "BottomUp");
+//
+//        System.out.println("Query + " + queryId);
+//    }
+//
+//    static class InputEvent {
+//        String data;
+//        String cameraId;
+//        String timestamp;
+//        int rows;
+//        int cols;
+//        int type;
+//    }
+//
+//
+//    static class OutputEvent {
+//        String data;
+//        String cameraId;
+//        String timestamp;
+//        int rows;
+//        int cols;
+//        int type;
+//    }
+//
+//
+//    static class SimilarityProcessMapper implements MapFunction<InputEvent, OutputEvent> {
+//        public OutputEvent map(final InputEvent inputEvent) {
+//            OutputEvent outputEvent =  new OutputEvent();
+//            // Mapping logic here
+//            return outputEvent;
+//        }
+//    }
+//
+//    private static void readFromKafka() throws  Exception {
+//        NebulaStreamRuntime ner = NebulaStreamRuntime.getRuntime();
+//        ner.getConfig().setHost("212.101.173.11").setPort("8081");
+//        System.out.println(" *** connected:\n");
+//
+//        Query w1 = ner.readFromSource("kafka_source"); //here should it be replaced with kafka_stream? TODO
+//
+//        w1.sink(new KafkaSink("example-output", "212.101.173.100:29092", 100));
+//        int queryId = ner.executeQuery(w1, "BottomUp");
+//
+//        System.out.println("Query + " + queryId);
+//        System.out.println(" *** \n");
+//    }
 
 
     // Save image file
@@ -125,9 +201,9 @@ public class SimilarityProcessor {
 
     private static Mat getMat(VideoEventStringProcessed ed) throws Exception {
         /*
-        * The mat object needs to match
-        * the rows and cols of indarray of network
-        */
+         * The mat object needs to match
+         * the rows and cols of indarray of network
+         */
         Mat mat = new Mat(ed.getRows(), ed.getCols(), ed.getType());
         mat.put(0, 0, Base64.getDecoder().decode(ed.getData()));
         return mat;
@@ -150,7 +226,7 @@ public class SimilarityProcessor {
     public static void addInitialMember(File imageFile, String name) throws IOException {
         String title = name ;
         if (StringUtils.isBlank(name)) {
-                 title = imageFile.getName().replaceAll("_", "")
+            title = imageFile.getName().replaceAll("_", "")
                     .replace(".jpg", "")
                     .replace(".png", "")
                     .replaceAll("[0-9]", "");
